b176c6932c696aef105fde9f58804ec413791b43
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2024-02-13 16:59:34 +0530

    recheck delete employee route

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -6,6 +6,6 @@ const route = express.Router();
 route.post('/create', create);
 route.get("/getallemployees" , fetch);
 route.put("/update/:id", update);
-route.delete("delete/:id", deleteEmployee);
+route.delete("/delete/:id", deleteEmployee);
 
 module.exports = route;
